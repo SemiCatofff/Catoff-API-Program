2f5c5797b66f6bddab8a42bb626cbb69e87b782d
Abhishek <109427449+decentralized-86@users.noreply.github.com>
2024-03-14 15:30:51 +0530

    changed state Accounts

--- programs/api_program/src/state.rs
+++ programs/api_program/src/state.rs
@@ -1,27 +1,8 @@
 use anchor_lang::prelude::*;
 
 #[account]
-pub struct Challenge {
-    pub escrow_account: Pubkey,
-    pub is_active: bool,
-}
-
-#[account]
-pub struct Participant {
-    pub user: Pubkey,
-    pub challenge_id: Pubkey,
-    pub entry_fee_paid: u64,
-}
-
-#[account]
-pub struct GlobalState {
-    pub admin: Pubkey,
-    pub sol_to_credit_exchange_rate: u64,
-}
-
-#[account]
-pub struct WithdrawalRequest {
-    pub requestor: Pubkey, // User requesting the withdrawal
-    pub amount: u64,       // Amount of SOL to withdraw, calculated based on credits burned
-    pub processed: bool,   // Whether the request has been processed by the off-chain service
+pub struct EscrowAccount {
+    pub sol_balance: u64, // Tracks the SOL balance if you need to track this separately.
+    pub usdt_balance: u64, // Placeholder for USDT balance, actual tracking will be through the token account.
+    pub authority: Pubkey, // Authority who can initiate withdrawals.
 }
