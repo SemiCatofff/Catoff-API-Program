723e66cd633889016f1cfd7378dc995c167ac441
semi-infiknight <mathurshubhang2002@gmail.com>
2024-03-22 11:07:55 +0530

    escrow initialization fixed

--- programs/api_program/src/state.rs
+++ programs/api_program/src/state.rs
@@ -3,9 +3,9 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct EscrowAccount {
-    pub sol_balance: u64, // Tracks the SOL balance if you need to track this separately.
-    pub usdt_balance: u64, // Placeholder for USDT balance, actual tracking will be through the token account.
-    pub authority: Pubkey, // Authority who can initiate withdrawals.
+    pub sol_balance: u64,
+    pub usdt_balance: u64,
+    pub authority: Pubkey,
 }
 #[event]
 pub struct DepositEvent {
