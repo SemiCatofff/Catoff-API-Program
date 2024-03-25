0056a340e1f9d421b7ea114ec10758218096abc8
Abhishek <109427449+decentralized-86@users.noreply.github.com>
2024-03-25 17:10:59 +0530

    new -program-changes deployment done

--- programs/api_program/src/lib.rs
+++ programs/api_program/src/lib.rs
@@ -4,7 +4,7 @@ mod state;
 
 use anchor_lang::prelude::*;
 
-declare_id!("CbkPd8y8rgVaAgfGo6TmiPXd5JGViVbbZW6WBCs8aD9V");
+declare_id!("79VSBMCvvTKNwZSocidYCTxDEDqy39ge9fdzWiBcP5WV");
 
 #[program]
 pub mod api_program {
